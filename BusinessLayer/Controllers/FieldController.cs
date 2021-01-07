a9d750b70d371600208f53599ce46070dac6db2a
FilipJQ77 <filipprzygonski99@gmail.com>
2021-01-07 17:20:10 +0100

    ffs git

---
+++ BusinessLayer/Controllers/FieldController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.BusinessObjects.Entities;
+using DataAccess.Repositories;
+
+namespace BusinessLayer.Controllers
+{
+    class FieldController
+    {
+        public Dictionary<string, string> showField(Dictionary<string, string> data, string token)
+        {
+            if (new UserController().CheckRank(token) != Rank.Administrator)
+                return new Dictionary<string, string>
+                {
+                    ["error"] = "Brak uprawnień do przeglądania kierunków",
+                };
+
+            if (!data.ContainsKey("id") || !int.TryParse(data["id"], out int id) || id < 1)
+                return new Dictionary<string, string>
+                {
+                    ["error"] = "Nieprawidłowe id",
+                };
+
+            var fRepo = new RepositoryFactory().GetRepository<Field>();
+            var field = fRepo.GetDetail(p => p.Id == id);
+
+            if (field == null)
+                return new Dictionary<string, string>
+                {
+                    ["error"] = "Kurs o podanym id nie istnieje",
+                };
+
+            return new Dictionary<string, string>
+            {
+                ["name"] = field.Name,
+                ["id"] = field.Id.ToString(),
+            };
+        }
+    }
+}
---
+++ BusinessLayer/Controllers/LessonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using BusinessLayer.Users;
+using DataAccess.BusinessObjects.Entities;
+using DataAccess.Repositories;
+
+namespace BusinessLayer.Controllers
+{
+    public class LessonController
+    {
+        public (bool, string) SignToLesson(Dictionary<string, string> data, string token)
+        {
+            var userRank = new UserController().CheckRank(token);
+
+            int studentId;
+            Student student;
+
+            switch (userRank)
+            {
+                case Rank.None:
+                    return (false, "Należy się zalogować");
+                case Rank.Administrator:
+                    if (!data.ContainsKey("studentId") || !int.TryParse(data["studentId"], out studentId))
+                        return (false, "Niepoprawne ID studenta");
+                    break;
+            }
+            var repoFactory = new RepositoryFactory();
+            var studentRepo = repoFactory.GetRepository<Student>();
+            var student = studentRepo.GetDetail();
+
+            var loggedUser = LoggedUsers
+                .GetInstance()
+                .GetUser(token);
+
+            var xd = new CourseGroupController().CheckPermissions(loggedUser);
+        }
+    }
+}
