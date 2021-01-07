b0f1a95cf17951e84c80f3657094cf069d612537
FilipJQ77 <filipprzygonski99@gmail.com>
2021-01-07 17:59:41 +0100

    sign to lesson should work lmao

--- BusinessLayer/Controllers/LessonController.cs
+++ BusinessLayer/Controllers/LessonController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BusinessLayer.Users;
 using DataAccess.BusinessObjects.Entities;
 using DataAccess.Repositories;
@@ -11,8 +12,16 @@ namespace BusinessLayer.Controllers
         {
             var userRank = new UserController().CheckRank(token);
 
+            var loggedUser = LoggedUsers
+                .GetInstance()
+                .GetUser(token);
+
+            var repoFactory = new RepositoryFactory();
+            var studentRepo = repoFactory.GetRepository<StudentData>();
+            var lessonRepo = repoFactory.GetRepository<Lesson>();
+
             int studentId;
-            Student student;
+            StudentData student;
 
             switch (userRank)
             {
@@ -21,17 +30,39 @@ namespace BusinessLayer.Controllers
                 case Rank.Administrator:
                     if (!data.ContainsKey("studentId") || !int.TryParse(data["studentId"], out studentId))
                         return (false, "Niepoprawne ID studenta");
+                    student = studentRepo.GetDetail(s => s.Id == studentId);
+                    break;
+                default: // student
+                    student = (loggedUser as Student).StudentData;
                     break;
             }
-            var repoFactory = new RepositoryFactory();
-            var studentRepo = repoFactory.GetRepository<Student>();
-            var student = studentRepo.GetDetail();
 
-            var loggedUser = LoggedUsers
-                .GetInstance()
-                .GetUser(token);
+            if (!data.ContainsKey("lessonId") || !int.TryParse(data["lessonId"], out int lessonId))
+                return (false, "Niepoprawne ID zajęć");
 
-            var xd = new CourseGroupController().CheckPermissions(loggedUser);
+            var lesson = lessonRepo.GetDetail(l => l.Id == lessonId);
+            if (lesson == null)
+                return (false, "Nie ma lekcji o podanym ID");
+            
+            var course = lesson.Course;
+            var courseGroup = lesson.Course.CourseGroup;
+            var (canSign, err) = new CourseGroupController().CheckPermissions(loggedUser,courseGroup.Id);
+            if (!canSign)
+                return (false, err);
+
+            if (student.Lessons.Any(l => l.Course == course))
+                return (false, "Student jest już zapisany na kurs");
+
+            if (lesson.Space <= 0 || userRank != Rank.Administrator)
+                return (false, "Brak miejsc");
+
+            student.Lessons.Add(lesson);
+            lesson.Space--;
+
+            studentRepo.SaveChanges();
+            lessonRepo.SaveChanges();
+
+            return (true, $"Zapis na zajęcia udał się");
         }
     }
 }
