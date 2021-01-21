5bef2bd71b6d940c00934b8d8622d1f0cbc80fa1
JeLLek <jellek@wp.pl>
2021-01-21 17:11:15 +0100

    fitness

---
+++ BusinessLayer/Tests/fitnesse/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fitlibrary;
+
+namespace BusinessLayer.Tests.fitnesse
+{
+    public class ColumnFixtureTest : fit.ColumnFixture
+    {
+        public String firstPart;
+        public String secondPart;
+        public String XD;
+        public String Together
+        {
+            get
+            {
+                return firstPart + ", " + secondPart + ", " + XD;
+            }
+        }
+        public int TotalLength()
+        {
+            return firstPart.Length + secondPart.Length + XD.Length;
+        }
+    }
+}
---
+++ fitness/run.cmd
@@ -0,0 +1,2 @@
+java -jar fitness.jar -p 8080 -d "../" -r fitNesseRoot
+pause
